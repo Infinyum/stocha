b2292fc9ee1bec21b6865b506422671a3fb0cb4d
Robin <malmasson.robin@gmail.com>
2019-12-07 17:52:38 +0100

    Hotfix: getters in Station

--- src/VLS/Station.java
+++ src/VLS/Station.java
@@ -55,6 +55,26 @@ public class Station {
 		this.w = w;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setK(int k) {
+		this.k = k;
+	}
+
 	
 	
 }
